2af6e082228fa9745a46de87bf43fb6c38cd2a40
Brown <preciousebere090@gmail.com>
2025-08-20 07:54:03 +0100

    updated

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,15 @@
 import { ReactNode } from "react";
+import type { Metadata } from "next";
+
 import { redirect } from "next/navigation";
 
 import { isAuthenticated } from "@/lib/actions/auth.action";
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: " An  AI-powered 🤖 platform for preparing for mock interviews, designed to simulate real interview environments where users can practice answering questions in real time. The application integrates voice and conversational AI via WebRTC and Vapi workflows, ensuring an interactive and immersive experience. Building such a system required seamlessly connecting multiple services: managing authentication and user data with Firebase, leveraging Gemini AI for dynamic question generation and feedback, and integrating Vapi for voice-based interview simulations. The result is a streamlined, professional platform that empowers users to prepare for interviews with greater confidence and realism.",
+};
+
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import type { Metadata } from "next";
 import Image from "next/image";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 import SignOutBtn from "@/components/ui/signOutBtn";
 
 import { isAuthenticated, signOut } from "@/lib/actions/auth.action";
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: " An  AI-powered 🤖 platform for preparing for mock interviews, designed to simulate real interview environments where users can practice answering questions in real time. The application integrates voice and conversational AI via WebRTC and Vapi workflows, ensuring an interactive and immersive experience. Building such a system required seamlessly connecting multiple services: managing authentication and user data with Firebase, leveraging Gemini AI for dynamic question generation and feedback, and integrating Vapi for voice-based interview simulations. The result is a streamlined, professional platform that empowers users to prepare for interviews with greater confidence and realism.",
+};
 
 const RootLayout = async ({children}: {children: ReactNode}) => {
   const isUserAuthenticated = await isAuthenticated();
